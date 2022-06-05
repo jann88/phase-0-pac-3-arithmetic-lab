1a80ff69d36db49bee2fb059754b0dc1bf267c74
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-06-05 23:07:18 +0300

    working on it

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+let number = 5;
+
+function number() {
+    return 5
+}
+
+function increment() {
+    function add2() {
+        return (number += 7);
+    }
+}
+
+function decrement() {
+    function subtract3() {
+        return (number -= 4);
+
+    }
+}
